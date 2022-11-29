03275fffe38f2cc17b4c3c595cfa58558c7ebed6
Vikas Yadav <vikasyadav3935@gmail.com>
2022-11-29 22:25:42 +0530

    29-11-22

--- src/Componets/Login.jsx
+++ src/Componets/Login.jsx
@@ -1,50 +1,118 @@
 import React from "react";
-import "./Login.css";
+import { useState } from "react";
 import {
   Modal,
   ModalOverlay,
   ModalContent,
   ModalBody,
   ModalCloseButton,
+  ModalHeader,
   Button,
   Image,
   Box,
   Heading,
   Input,
+  HStack,
+  Icon,
 } from "@chakra-ui/react";
+import { Checkbox, } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
+import {IoLogoWhatsapp} from "react-icons/io";
 
 const Login = () => {
+  const [user, setUser] = useState("@");
+  const [btn, setbtn] = useState();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handlechange = (e) => {
+    setUser(e.target.value);
+    console.log(user.includes("@gmail."));
+    // let val = user.includes("@gmail.");
+    // setbtn(val);
+     const buton= <Box
+     fontSize={"12px"}
+     mt="18px"
+     color={"#ff1f1f"}
+     fontWeight="500"
+     letterSpacing={"-0.4px"}
+   >
+     Please enter a valid Email or Mobile Number.
+   </Box>
+   setbtn(buton);
+  
+  };
   return (
     <div>
       <Button onClick={onOpen}>Sign In</Button>
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent>
-          <ModalCloseButton />
-          <ModalBody className="modalBody">
+        <ModalHeader></ModalHeader>
+        <ModalContent w={"420px"}>
+          <ModalCloseButton borderRadius={"50%"}  bg="white"/>
+          <ModalBody p={"0px 0px "} borderRadius={"15px 15px 15px 15px "}>
             <Image
               src="https://static1.lenskart.com/media/desktop/img/DesignStudioIcons/DesktopLoginImage.svg"
               alt="pic"
-              className="loginImage"
+              // id="loginImage"
+              borderRadius={"10px 10px 0px 0px "}
             />
-            <Box className="secbox">
-              <Heading className="heading1">Sign In</Heading>
-              <Input placeholder="Mobile/Email" className="input1" />
-              <Box className="label0">
-                <input type="checkbox" className="label2" />
-                <label className="label1">Get update on whatsapp </label>
-                <br />
-              </Box>
+            <Box m={"34px 45px 50px 45px"}>
+              <Heading
+                fontFamily={" Times, serif"}
+                fontWeight="100"
+                fontSize={"26px"}
+                mb="24px"
+                color={"#333368"}
+              >
+                Sign In
+              </Heading>
 
-              <Button className="btn1">Sign In</Button>
-              <Box className="footer">
-                <Box>New member?</Box>
-                <Link>Create an Account</Link>
+              <Input
+                placeholder="Mobile/Email"
+                h={"50px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                onChange={handlechange}
+              />
+              {user.includes("@gmail.") ? (
+                ""
+              ) : (
+               btn
+              )}
+
+              <Box fontSize={"12px"}>
+                <Checkbox mb={"20px"} mt="20px" size="sm">
+                  Get Update on whatsapp
+                </Checkbox>
               </Box>
+              {user.includes("@gmail.") ? (
+                <Button
+                  bgColor={"#11daac"}
+                  width="100%"
+                  borderRadius={"35px/35px"}
+                  h="50px"
+                  _hover={{ backgroundColor: "#11daac" }}
+                >
+                  Sign In
+                </Button>
+              ) : (
+                <Button
+                  bgColor={"#cccccc"}
+                  width="100%"
+                  borderRadius={"35px/35px"}
+                  h="50px"
+                  _hover={{ backgroundColor: "#cccccc" }}
+                >
+                  Sign In
+                </Button>
+              )}
+
+              <HStack spacing={"0px"} mt="19px">
+                <Box fontSize={"14px"}> New member?</Box>
+                <Link fontSize={"15px"} fontWeight="500" textDecoration={"underline"}>Create an Account</Link>
+              </HStack>
             </Box>
           </ModalBody>
         </ModalContent>
--- src/Componets/Signup.jsx
+++ src/Componets/Signup.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import "./Login.css";
-import { Heading, useDisclosure } from "@chakra-ui/react";
+// import "./Login&sign.css";
+import {
+  Center,
+  Heading,
+  HStack,
+  InputGroup,
+  InputLeftElement,
+  ModalHeader,
+  useDisclosure,
+} from "@chakra-ui/react";
 import {
   Modal,
   ModalOverlay,
@@ -10,33 +18,239 @@ import {
   Button,
   Box,
   Input,
+  Checkbox,
 } from "@chakra-ui/react";
+import { useState } from "react";
+import Required from "./Required";
 
 const Signup = () => {
+  const init = {
+    first_name: "",
+    last_name: "",
+    ph_no: "",
+    email: "",
+    password: "",
+  };
+
+  const [userData, setUserData] = useState(init);
+  const [first, setFirst] = useState();
+  const [ph, setPh] = useState();
+  const [mail, setMail] = useState();
+  const [pass, setPass] = useState();
+
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUserData({ ...userData, [name]: value });
+
+    switch (name) {
+      case "first_name":
+        setFirst(value === "" ? <Required info="This is required" /> : "");
+
+        break;
+
+      case "ph_no":
+        setPh(
+          value === "" ? (
+            <Required info="This is required" />
+          ) : (
+            <Required info="Please enter a valid mobile number (eg. 9987XXXXXX)" />
+          )
+        );
+
+        break;
+
+      case "email":
+        setMail(
+          value === "" ? (
+            <Required info="This is required" />
+          ) : (
+            <Required info="Please enter a valid email address e.g. johndoe@domain.com." />
+          )
+        );
+
+        break;
+
+      case "password":
+        setPass(
+          value === "" ? (
+            <Required info="This is required" />
+          ) : (
+            <Required info="Password should be more than 6 characters." />
+          )
+        );
+
+        break;
+
+      default:
+        break;
+    }
+   
+  };
+
+  const getData=(body)=>{
+    fetch(`https://lenskart-clone.herokuapp.com/Users`,{
+      method:"POST",
+      body:JSON.stringify(body),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+  }
+
+  const handleRegister=()=>{
+    getData(userData);
+
+
+  }
+
+  console.log(userData);
   return (
     <div>
       <Button onClick={onOpen}>Sign Up</Button>
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent>
+        <ModalHeader></ModalHeader>
+        <ModalContent w={"430px"}>
           <ModalCloseButton />
-          <ModalBody>
-            <Box className="signup">
-              <Heading className="heading1">Create an Account</Heading>
-              <Input placeholder="First Name*" className="input1" />
-              <Input placeholder="Last Name" className="input1" />
-              <Input placeholder="+91 Mobile*" className="input1" />
-              <Input placeholder="Email*" className="input1" />
-              <Input placeholder="Password*" className="input1" />
+          <ModalBody p={"0px 0px "}>
+            <Box m={"5px 45px 20px 45px"}>
+              <Heading
+                fontFamily={" Times, serif"}
+                fontWeight="100"
+                fontSize={"26px"}
+                mb="20px"
+                color={"#333368"}
+              >
+                Create an Account
+              </Heading>
+              <Input
+                onChange={handleChange}
+                
+                type="text"
+                name="first_name"
+                placeholder="First Name*"
+                h={"49px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                m={"8px 0px 8px 0px"}
+              />
+              {first}
 
-              <Box className="label0">
-                <input type="checkbox" className="label2" />
-                <label className="label1">Get update on whatsapp</label>
-                <br />
+              <Input
+                onChange={handleChange}
+                name="last_name"
+                placeholder="Last Name"
+                h={"49px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                m={"8px 0px 8px 0px"}
+              />
+              <InputGroup>
+              <InputLeftElement children="+91 " mt={"12px"} ml="10px" pr={"10px"}/>
+              <Input
+                onChange={handleChange}
+                type="number"
+                name="ph_no"
+                placeholder=" Mobile*"
+                h={"49px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                m={"8px 0px 8px 0px"}
+              />
+
+              </InputGroup>
+             
+              {userData.ph_no.length == 10 ? "" : ph}
+              <Input
+                onChange={handleChange}
+                name="email"
+                placeholder="Email*"
+                h={"49px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                m={"8px 0px 8px 0px"}
+              />
+              {userData.email.includes("@gmail.") ? "" : mail}
+              <Input
+                onChange={handleChange}
+                type={"password"}
+                name="password"
+                placeholder="Password*"
+                h={"49px"}
+                focusBorderColor="rgb(206, 206, 223)"
+                borderColor={"rgb(206, 206, 223)"}
+                m={"8px 0px 8px 0px"}
+              />
+              {userData.password.length >= 6 ? "" : pass}
+              <Box
+                textDecoration={"underline"}
+                fontFamily={" sans-serif"}
+                color={"#333368"}
+                fontSize="14px"
+              >
+                Got a Referral code?
               </Box>
-              <Button className="btn1">Create an Account</Button>
+
+              <Checkbox
+                mb={"20px"}
+                mt="20px"
+                size="sm"
+                fontFamily={" sans-serif"}
+              >
+                Get Update on whatsapp
+              </Checkbox>
+
+              <HStack spacing={"3px"} mb="10px">
+                <Box
+                  fontSize={"14px"}
+                  fontFamily={" sans-serif"}
+                  fontWeight="100"
+                  letterSpacing={"-0.4px"}
+                >
+                  By creating this account, you agree to our
+                </Box>
+                <Box fontSize={"15px"} textDecoration="underline">
+                  Privacy Policy
+                </Box>
+              </HStack>
+
+              {userData.email.includes("@gmail.") &&
+              userData.password.length >= 6 &&
+              userData.ph_no.length == 10 ? (
+                <Button
+                onClick={handleRegister}
+                  bgColor={"#11daac"}
+                  width="100%"
+                  borderRadius={"35px/35px"}
+                  h="50px"
+                  _hover={{ backgroundColor: "#11daac" }}
+                  fontFamily={" sans-serif"}
+                  fontWeight="300"
+                >
+                  Create an Account
+                </Button>
+              ) : (
+                <Button
+                  bgColor={"#cccccc"}
+                  width="100%"
+                  borderRadius={"35px/35px"}
+                  h="50px"
+                  _hover={{ backgroundColor: "#cccccc" }}
+                  fontFamily={" sans-serif"}
+                  fontWeight="300"
+                >
+                  Create an Account
+                </Button>
+              )}
+              <Center mt={"14px"} fontSize="15px">
+                Have an account?{" "}
+                <Center fontWeight={"500"} textDecoration="underline">
+                  Sign In
+                </Center>
+              </Center>
             </Box>
           </ModalBody>
         </ModalContent>
