614672814da136bada4c3c89472f246c40d27524
Amit Kumar <amitmishra74880@gmail.com>
2022-12-01 17:23:43 +0530

    commit

--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import CartPage from './CartPage'
 import HomePage from './HomePage'
+import Account from '../Componets/Account'
+import MyOrders from './MyOrders'
+import { Box } from '@chakra-ui/react'
 
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/customer' element={<MyOrders/>}>
+          <Route path='account' element={<Account/>}/>
+          <Route path='notification' element={<Box fontSize={"40px"}>Manage Notification</Box>}/>
+        </Route>
         <Route path='/cart' element={<CartPage/>}/>
     </Routes>
   )
