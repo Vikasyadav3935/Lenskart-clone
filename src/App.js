01342097cd3808749f0721abe8bf73420eea36ee
Rajatsinha05 <rajatsinha5467@gmail.com>
2022-12-01 18:04:40 +0530

    files changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import AllRoutes from "./Pages/AllRoutes";
-import Login from "./Componets/Login";
-import Signup from "./Componets/Signup";
-import MyOrders from "./Pages/MyOrders";
-import Account from "./Componets/Account";
+
 
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
-      {/* <Login/>
-      <Signup/> */}
-      {/* <MyOrders/>
-      <Account/> */}
-
+    
     </div>
   );
 }
