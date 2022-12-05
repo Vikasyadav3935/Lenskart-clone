96fd7539e95eca40ded7f4f4d003835ce6e5c46b
vinay <vinaykhairnar9404@gmail.com>
2022-12-05 09:40:09 +0530

    final

--- src/Componets/ProductComponents/FrameType.jsx
+++ src/Componets/ProductComponents/FrameType.jsx
@@ -1,9 +1,9 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const FrameType = ({src , type}) => {
+const FrameType = ({src , type ,fil , setfilter}) => {
   return (
-      <Box mr="3px" border="1px solid" borderColor="gray.300">
+      <Box onClick={()=>setfilter((pre)=>[...pre , fil])} mr="3px" border="1px solid" borderColor="gray.300">
         <Image m="7px auto" width="45px" src={src} />
         <Text mx="5px" textAlign="center">{type}</Text>
       </Box>
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -5,16 +5,19 @@ import HomePage from './HomePage'
 import Account from '../Componets/Account'
 import MyOrders from './MyOrders'
 import { Box } from '@chakra-ui/react'
+import ProductList from '../Componets/ProductComponents/ProductList'
+import SingleProductPage from './SingleProductPage'
 
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path='/' element={<ProductList/>}/>
         <Route path='/customer' element={<MyOrders/>}>
           <Route path='account' element={<Account/>}/>
           <Route path='notification' element={<Box fontSize={"40px"}>Manage Notification</Box>}/>
         </Route>
+          <Route path='/product/:id' element={<SingleProductPage/>}/>
         <Route path='/cart' element={<CartPage/>}/>
     </Routes>
   )
