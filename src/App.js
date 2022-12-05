211bd72dd5c77b18ba6239c824ac947b7554086b
yogeshvari2902 <yogeshvari2022@gmail.com>
2022-12-05 09:48:50 +0530

    done update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import AllRoutes from "./Pages/AllRoutes";
-import Login from "./Componets/Login";
-import Signup from "./Componets/Signup";
-import MyOrders from "./Pages/MyOrders";
-import Account from "./Componets/Account";
-// import TopNav from "./Componets/Navbar/TopNav";
+
 
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
-      {/* <Login/> */}
-      {/* <Signup/> */}
-      {/* <TopNav/> */}
-      {/* <MyOrders/>
-      <Account/> */}
-
+    
     </div>
   );
 }
