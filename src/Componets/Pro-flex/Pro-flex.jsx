7bdbd0f2421a5318c716d383ed7a79fb6515abc2
Rajatsinha05 <rajatsinha5467@gmail.com>
2022-12-05 09:32:33 +0530

    drop-down

--- src/Componets/Pro-flex/Pro-flex.jsx
+++ src/Componets/Pro-flex/Pro-flex.jsx
@@ -1,9 +1,124 @@
 import React from 'react'
+import '../Comp.css'
+function Proflex(props) {
+  console.log('props: ', props);
+ 
 
-function Pro-flex() {
+
+
+
+
   return (
-    <div>Pro-flex</div>
+    <div id="parent-pro-flex">
+
+
+
+<div className='pro-contains'>
+
+<div className='drop-contain'>
+<img src="https://static1.lenskart.com/media/desktop/img/Apr22/a2.png" alt="img"/>
+  <h5>Eyeglasses</h5>
+
+<div className='drop-opt'> 
+  
+<div>
+  
+  <div className='opt-flex'>
+  <h6>Air light-Weight</h6>
+
+<p>Staring</p>
+  </div>
+<div className='opt-flex'>
+  <h5>powered lenses</h5>
+
+<p>₹1199</p>
+
+
+</div>
+
+<div className='opt-flex'>
+  <h6>Air light-Weight</h6>
+
+<p>Staring</p>
+  </div>
+<div className='opt-flex'>
+  <h5>powered lenses</h5>
+
+<p>₹1199</p>
+
+
+</div>
+
+<div className='opt-flex'>
+  <h6>Air light-Weight</h6>
+
+<p>Staring</p>
+  </div>
+<div className='opt-flex'>
+  <h5>powered lenses</h5>
+
+<p>₹1199</p>
+
+
+</div>
+
+<div className='opt-flex'>
+  <h6>Air light-Weight</h6>
+
+<p>Staring</p>
+  </div>
+<div className='opt-flex'>
+  <h5>powered lenses</h5>
+
+<p>₹1199</p>
+
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+  
+
+
+
+
+
+
+
+  
+</div>
+
+
+  
+</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
   )
 }
 
-export default Pro-flex
+export default Proflex
