d94ad545bf615e869b817c8ca92aaa123ca526b0
yogeshvari2902 <yogeshvari2022@gmail.com>
2022-12-05 09:44:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Login from "./Componets/Login";
 import Signup from "./Componets/Signup";
 import MyOrders from "./Pages/MyOrders";
 import Account from "./Componets/Account";
+// import TopNav from "./Componets/Navbar/TopNav";
 
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
-      <Login/>
-      <Signup/>
+      {/* <Login/> */}
+      {/* <Signup/> */}
+      {/* <TopNav/> */}
       {/* <MyOrders/>
       <Account/> */}
 
--- src/Pages/AllRoutes.jsx
+++ src/Pages/AllRoutes.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './HomePage'
+import TopNav from '../Componets/Navbar/TopNav';
 import Account from '../Componets/Account'
 import MyOrders from './MyOrders'
 import { Box } from '@chakra-ui/react'
-
+import Footer from '../Componets/Footer/Footer';
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/customer' element={<MyOrders/>}>
-          <Route path='account' element={<Account/>}/>
-          <Route path='notification' element={<Box fontSize={"40px"}>Manage Notification</Box>}/>
-        </Route>
+        {/* <Route path='/customer' element={<MyOrders/>}>
+        <Route path='account' element={<Account/>}/> */}
+        <Route path='/Navbar' element={<TopNav/>}/>
+        <Route path='/Footer' element={<Footer/>}/>
+        {/* <Route path='notification' element={<Box fontSize={"40px"}>Manage Notification</Box>}/> */}
+        {/* </Route> */}
     </Routes>
   )
 }
